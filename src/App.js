b80fe0f1b8a3d01e444c4d096c0d2af24ab380d3
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2024-08-26 23:24:17 +0900

    feat: post 실험

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import PostList from './components/posts/PostList';
+
+function App() {
+  return (
+    <div className="App">
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
+
